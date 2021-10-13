b94f10803fa01bb8c6aeb3c3fc0bd61c88c7eff9
gonAccount <35288746+gonAccount@users.noreply.github.com>
2021-10-13 20:16:26 +0200

    Update DataOperations.java

--- src/DataOperations.java
+++ src/DataOperations.java
@@ -6,6 +6,9 @@ import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 
 public class DataOperations {
@@ -13,16 +16,19 @@ public class DataOperations {
     JPanel jPanel;
     int rowWidth;
     int rowHeight;
+    String savePath;
 
     public DataOperations(String savePath, JPanel jPanel, int rowWidth, int rowHeight) throws IOException {
         this.defaultTableModel = initializeTable(savePath);
         this.jPanel = jPanel;
         this.rowWidth = rowWidth;
         this.rowHeight = rowHeight;
+        this.savePath = savePath;
     }
 
     public static DefaultTableModel initializeTable(String savePath) throws IOException {
         Object[] colNames = {"URL", "Name", "Creation Time", "Current Status"};
+
         FileReader fileReader = new FileReader(savePath);
         BufferedReader in = new BufferedReader(fileReader);
 
@@ -31,7 +37,7 @@ public class DataOperations {
 
         for(int i = 0; i < savedDataList.size(); i++) {
             Object[] split = savedDataList.get(i).split(";");
-            split[3] = ServiceActionListener.getResponseCondition(split[0].toString());
+            split[3] = getResponseCondition(split[0].toString());
 
             savedDataArray[i] = split;
         }
@@ -64,6 +70,25 @@ public class DataOperations {
                 creationTime,
                 httpStatus};
         defaultTableModel.addRow(newRow);
+    }
+
+    public static String getResponseCondition(String fieldUrl) throws IOException {
+        int httpStatus = getResponseCode(fieldUrl);
+        String httpCondition;
+        if((httpStatus == 200)) {
+            httpCondition = "OK";
+        }else {
+            httpCondition = "FAIL";
+        }
+        return httpCondition;
+    }
+
+    private static int getResponseCode(String urlPath) throws IOException {
+        URL url = new URL(urlPath);
+        URLConnection urlConnection = url.openConnection();
+
+        HttpURLConnection httpURLConnection = (HttpURLConnection) urlConnection;
+        return httpURLConnection.getResponseCode();
     }
 
 }
